f6ebbeb753cbdb79638382199feba17200cf6ed6
jaclyn16 <jgrix16@yahoo.com>
2026-04-13 13:45:23 -0700

    updated redirects/vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://product-feedback-app-7kxc.onrender.com',
         changeOrigin: true
       }
     }
